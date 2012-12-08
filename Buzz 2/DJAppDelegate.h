62c95fb2c46419ee587383846aa9b1cbd7e4ab0a
Earl <earltagra@Bolt.local>
2012-12-08 12:13:41 +0800

    Initial Commit

---
+++ Buzz 2/DJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DJAppDelegate.h
+//  Buzz 2
+//
+//  Created by Earl on 12/8/12.
+//  Copyright (c) 2012 Earl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DJAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
